9920e6a5d7cdb8e30a3d6567902cad212f5a9c08
Almousa mohanad <almousa.m75@gmail.com>
2018-03-18 14:08:18 +0100

    créer La fonction animer et finaliser le script balle_rebondit.js

--- balle_rebondit.js
+++ balle_rebondit.js
@@ -20,3 +20,23 @@ window.onload = function()
     		var vitesseX = 5;
     		var vitesseY = 5;
  	      var myInterval = setInterval(animer, 1000/30);
+        function animer()
+
+          {
+             context.clearRect(0, 0, canvas.width, canvas.height);
+                  
+             context.beginPath();
+             context.arc(posX, posY, diametreBalle/2, 0, Math.PI*2);
+             context.fill(); 
+             if(posX+diametreBalle/2 >= canvas.width || posX <= 0+diametreBalle/2)
+              {
+                  vitesseX *= -1;
+              }
+             if(posY+diametreBalle/2 >= canvas.height || posY <= 0+diametreBalle/2) 
+                {
+                  vitesseY *= -1;
+              }
+                 posX += vitesseX;
+               posY += vitesseY;
+         }
+    }
